c620bbf6d7382a09fdf9e40623511ecb794c91c9
Kief <keithsedgeman@gmail.com>
2021-05-22 20:13:48 +0100

    Prop Cannon work

--- Assets/Editor/PropCannon.cs
+++ Assets/Editor/PropCannon.cs
@@ -4,38 +4,48 @@ using UnityEngine.Rendering;
 
 public class PropCannon : EditorWindow
 {
+    // Create menu item
     [MenuItem("Tools/Prop Cannon")]
     public static void OpenPropCannonWindow() => GetWindow<PropCannon>();
 
+    // Brush size
     [Range(0.01f, 50f)]
     public float radius = 2f;
 
+    // Point density
     [Range(1f, 256f)]
     public int spawnCount = 8;
 
-    // Serialized Properties
+    // Prop prefabs
+    public GameObject spawnPrefab = null;
+
+    // Set up Serialized Properties
     private SerializedObject serializedObject;
     private SerializedProperty pRadius;
     private SerializedProperty pSpawnCount;
+    private SerializedProperty pSpawnPrefab;
 
+    // A place to store the points
     private Vector2[] randomPoints;
 
+    // When Window is opened
     private void OnEnable()
     {
-        // Serialized Properties
+        // Assign Serialized Properties
         serializedObject = new SerializedObject(this);
         pRadius = serializedObject.FindProperty("radius");
         pSpawnCount = serializedObject.FindProperty("spawnCount");
-
+        pSpawnPrefab = serializedObject.FindProperty("spawnPrefab");
 
         // Load config
         radius = EditorPrefs.GetFloat("PROP_CANNON_RADIUS", 2f);
         spawnCount = EditorPrefs.GetInt("PROP_CANNON_SPAWN_COUNT", 8);
 
+        GenerateRandomPoints();
         SceneView.duringSceneGui += DuringSceneViewGUI;
-        //autoRepaintOnSceneChange = true;
     }
 
+    // When the window is closed
     private void OnDisable()
     {
         // Save config
@@ -54,6 +64,7 @@ public class PropCannon : EditorWindow
         pRadius.floatValue = pRadius.floatValue.AtLeast(0.1f);
         EditorGUILayout.PropertyField(pSpawnCount);
         pSpawnCount.intValue = pSpawnCount.intValue.AtLeast(1);
+        EditorGUILayout.PropertyField(pSpawnPrefab);
 
         // Repaint Scene View if changes detected
         if (serializedObject.ApplyModifiedProperties())
@@ -62,7 +73,7 @@ public class PropCannon : EditorWindow
             SceneView.RepaintAll();
         }
 
-        // If mouse clicked in editor window
+        // If mouse clicked in editor window, clear focus
         if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
         {
             GUI.FocusControl(null);
@@ -72,39 +83,84 @@ public class PropCannon : EditorWindow
 
     private void DuringSceneViewGUI(SceneView sceneView)
     {
+        // See Handles through objects
+        Handles.zTest = CompareFunction.Always;
+
         // Run once per frame
-        if (Event.current.type != EventType.Repaint) return;
+        //if (Event.current.type != EventType.Repaint) return;
 
         // Get the Camera Transform
         Transform camTf = sceneView.camera.transform;
 
-        // Cast a Ray through Camera
-        Ray ray = new Ray(camTf.position, camTf.forward);
+        // Update Scene View when moving mouse
+        if (Event.current.type == EventType.MouseMove) { SceneView.RepaintAll(); }
 
-        // If Ray hits something
+        // Check if Shift or Ctrl is pressed
+        bool holdingShift = (Event.current.modifiers & EventModifiers.Shift) != 0;
+        bool holdingCtrl = (Event.current.modifiers & EventModifiers.Control) != 0;
+
+        // Change radius and Spawn Count
+        if (Event.current.type == EventType.ScrollWheel)
+        {
+            // Get roll direction
+            float scrollDirection = Mathf.Sign(-Event.current.delta.y);
+
+            if (holdingShift)
+            {
+                // Apply to radius
+                serializedObject.Update();
+                pRadius.floatValue *= 1f + scrollDirection * 0.05f;
+                serializedObject.ApplyModifiedPropertiesWithoutUndo();
+                // Updates Editor Window
+                Repaint();
+
+                // Consume the event. Don't let it fall through
+                Event.current.Use();
+            }
+            else if (holdingCtrl)
+            {
+                // Apply to spawn count
+                serializedObject.Update();
+                pSpawnCount.intValue += 1 * (int) scrollDirection;
+                serializedObject.ApplyModifiedPropertiesWithoutUndo();
+                GenerateRandomPoints();
+                // Updates Editor Window
+                Repaint();
+
+                // Consume the event. Don't let it fall through
+                Event.current.Use();
+            }
+        }
+
+        // Draw Gizmo's and Ghost Props
+        Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            Handles.zTest = CompareFunction.Always;
-
             // Setting up tangent space
             Vector3 hNormal = hit.normal;
             Vector3 hTangent = Vector3.Cross(hNormal, camTf.up).normalized;
             Vector3 hBiTangent = Vector3.Cross(hNormal, hTangent);
 
+            Ray GetTangentRay(Vector2 tangentSpacePos)
+            {
+                Vector3 rayOrigin = hit.point +
+                                    (hTangent * tangentSpacePos.x + hBiTangent * tangentSpacePos.y) * radius;
+                rayOrigin += hNormal * 2;
+                Vector3 rayDirection = -hNormal;
+                return new Ray(rayOrigin, rayDirection);
+            }
+
             // Drawing points
             foreach (Vector2 point in randomPoints)
             {
-                // Create ray for this point
-                Vector3 rayOrigin = hit.point + (hTangent * point.x + hBiTangent * point.y) * radius;
-                rayOrigin += hNormal * 2;
-                Vector3 rayDirection = -hNormal;
+                // Raycast to find a point on surface
+                Ray ptRay = GetTangentRay(point);
 
-                // Raycast to find point on surface
-                Ray ptRay = new Ray(rayOrigin, rayDirection);
+                // If it hits something
                 if (Physics.Raycast(ptRay, out RaycastHit ptHit))
                 {
                     // Draw sphere and normal on surface
-                    DrawSphere(ptHit.point);
+                    DrawGhostProp(ptHit.point);
                     Handles.DrawAAPolyLine(ptHit.point, ptHit.point + ptHit.normal);
                 }
             }
@@ -117,10 +173,38 @@ public class PropCannon : EditorWindow
             Handles.color = Color.blue;
             Handles.DrawAAPolyLine(6f, hit.point, hit.point + hNormal);
 
+            // Draw circle adapted to terrain
+            const int CIRCLE_DETAIL = 256;
+            const float TAU = 6.28318530718f;
+            Vector3[] ringPoints = new Vector3[CIRCLE_DETAIL];
+            for (int i = 0; i < CIRCLE_DETAIL; i++)
+            {
+                float t = i / ((float) CIRCLE_DETAIL - 1);
+                float angRad = t * TAU;
+                Vector2 dir = new Vector2(Mathf.Cos(angRad), Mathf.Sin(angRad));
+                Ray r = GetTangentRay(dir);
+                if (Physics.Raycast(r, out RaycastHit cHit))
+                {
+                    ringPoints[i] = cHit.point + cHit.normal * 0.02f;
+                }
+                else
+                {
+                    ringPoints[i] = r.origin;
+                }
+            }
+
+            Handles.DrawAAPolyLine(ringPoints);
+
             // Draw Scene Handles
-            Handles.color = Color.black;
-            Handles.DrawWireDisc(hit.point, hit.normal, radius, 2f);
-            Handles.color = Color.white;
+            // Handles.color = Color.black;
+            // Handles.DrawWireDisc(hit.point, hit.normal, radius, 2f);
+            // Handles.color = Color.white;
+        }
+
+        // Spawn props
+        if (holdingCtrl && Event.current.type == EventType.MouseDown && Event.current.button == 0)
+        {
+            TrySpawnProps();
         }
     }
 
@@ -133,10 +217,15 @@ public class PropCannon : EditorWindow
         }
     }
 
-    void DrawSphere(Vector3 pos)
+    private static void DrawGhostProp(Vector3 pos)
     {
         Quaternion qid = Quaternion.identity;
         const EventType RP = EventType.Repaint;
         Handles.SphereHandleCap(-1, pos, qid, 0.1f, RP);
+    }
+
+    private void TrySpawnProps()
+    {
+        if (spawnPrefab == null) { return; }
     }
 }
--- Assets/ExtensionMethods.cs
+++ Assets/ExtensionMethods.cs
@@ -8,18 +8,16 @@ public static class ExtensionMethods
         return new Vector3(Mathf.Round(v.x), v.y, Mathf.Round(v.z));
     }
 
-    // Round Vector3 v to increments of float s
+    // Round to increments of float s
     public static Vector3 Round(this Vector3 v, float s) { return (v / s).Round() * s; }
-
-    // Round float v to increments of float s 
     public static float Round(this float v, float s) { return Mathf.Round(v / s) * s; }
+
+    public static int AtLeast(this int v, int min) { return Mathf.Max(v, min); }
+    public static float AtLeast(this float v, float min) { return Mathf.Max(v, min); }
+
+    public static int AtMost(this int v, int max) { return Mathf.Min(v, max); }
+    public static float AtMost(this float v, float max) { return Mathf.Min(v, max); }
 
     // Easy log - ie. "Hello".Log();
     public static void Log(this string s) { Debug.Log(s); }
-
-    public static float AtLeast(this float v, float min) { return Mathf.Max(v, min); }
-    public static int AtLeast(this int v, int min) { return Mathf.Max(v, min); }
-
-    public static float AtMost(this float v, float max) { return Mathf.Min(v, max); }
-    public static int AtMost(this int v, int max) { return Mathf.Min(v, max); }
 }
