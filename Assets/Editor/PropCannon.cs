a2de66cb4f88a05c35df8292d9f45be61a4e2efd
Kief <keithsedgeman@gmail.com>
2021-05-23 20:41:07 +0100

    Massive Refactor

--- Assets/Editor/PropCannon.cs
+++ Assets/Editor/PropCannon.cs
@@ -17,9 +17,9 @@ public class PropCannon : EditorWindow
     [Range(1f, 256f)]
     public int spawnCount = 8;
 
-    // Prop prefabs
+    // Prop prefab
     public GameObject spawnPrefab;
-    public Material previewMaterial;
+    //public Material previewMaterial;
 
     // Set up Serialized Properties
     private SerializedObject serializedObject;
@@ -29,8 +29,20 @@ public class PropCannon : EditorWindow
     private SerializedProperty pSpawnPrefab;
     //private SerializedProperty pPreviewMaterial;
 
-    // A place to store the points
-    private Vector2[] randomPoints;
+    // A place to store random placement data
+    private struct RawPlacement
+    {
+        public Vector2 Point2D;
+        public float AngleDegrees;
+
+        public void Randomize()
+        {
+            Point2D = Random.insideUnitCircle;
+            AngleDegrees = Random.value * 360;
+        }
+    }
+
+    private RawPlacement[] rawPlacements;
 
     // When Window is opened
     private void OnEnable()
@@ -62,6 +74,9 @@ public class PropCannon : EditorWindow
 
     private void OnGUI()
     {
+        // See Handles through objects
+        Handles.zTest = CompareFunction.Always;
+
         serializedObject.Update();
 
         // Add Serialized Properties to GUI
@@ -72,7 +87,7 @@ public class PropCannon : EditorWindow
         EditorGUILayout.PropertyField(pSpawnPrefab);
         //EditorGUILayout.PropertyField(pPreviewMaterial);
 
-        // Repaint Scene View if changes detected
+        // Repaint if changes detected
         if (serializedObject.ApplyModifiedProperties())
         {
             GenerateRandomPoints();
@@ -89,21 +104,12 @@ public class PropCannon : EditorWindow
 
     private void DuringSceneViewGUI(SceneView sceneView)
     {
-        // See Handles through objects
-        Handles.zTest = CompareFunction.Always;
-
-        // Run once per frame
-        //if (Event.current.type != EventType.Repaint) return;
-
-        // Get the Camera Transform
-        Transform camTf = sceneView.camera.transform;
-
         // Update Scene View when moving mouse
         if (Event.current.type == EventType.MouseMove) { SceneView.RepaintAll(); }
 
         // Check if Shift or Ctrl is pressed
-        bool holdingShift = (Event.current.modifiers & EventModifiers.Shift) != 0;
-        bool holdingCtrl = (Event.current.modifiers & EventModifiers.Control) != 0;
+        bool isHoldingShift = (Event.current.modifiers & EventModifiers.Shift) != 0;
+        bool isHoldingCtrl = (Event.current.modifiers & EventModifiers.Control) != 0;
 
         // Change radius and Spawn Count
         if (Event.current.type == EventType.ScrollWheel)
@@ -111,138 +117,174 @@ public class PropCannon : EditorWindow
             // Get roll direction
             float scrollDirection = Mathf.Sign(-Event.current.delta.y);
 
-            if (holdingShift)
+            if (isHoldingShift)
             {
                 // Apply to radius
                 serializedObject.Update();
                 pRadius.floatValue *= 1f + scrollDirection * 0.05f;
                 serializedObject.ApplyModifiedPropertiesWithoutUndo();
-                // Updates Editor Window
+                // Update Editor Window
                 Repaint();
 
-                // Consume the event. Don't let it fall through
+                // Consume event. Don't let it fall through
                 Event.current.Use();
             }
-            else if (holdingCtrl)
+            else if (isHoldingCtrl)
             {
                 // Apply to spawn count
                 serializedObject.Update();
                 pSpawnCount.intValue += 1 * (int) scrollDirection;
                 serializedObject.ApplyModifiedPropertiesWithoutUndo();
                 GenerateRandomPoints();
-                // Updates Editor Window
+                // Update Editor Window
                 Repaint();
 
-                // Consume the event. Don't let it fall through
+                // Consume event. Don't let it fall through
                 Event.current.Use();
             }
         }
 
+        RenderProps(sceneView, isHoldingCtrl);
+    }
+
+    private void RenderProps(SceneView sceneView, bool isHoldingCtrl)
+    {
+        // Get the Camera Transform
+        Vector3 sceneViewCameraTransformUp = sceneView.camera.transform.up;
+
         // Draw Gizmo's and Ghost Props
-        Ray ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit))
+        Ray mouseRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+        if (Physics.Raycast(mouseRay, out RaycastHit mouseRaycastHit))
         {
-            // Setting up tangent space
-            Vector3 hNormal = hit.normal;
-            Vector3 hTangent = Vector3.Cross(hNormal, camTf.up).normalized;
-            Vector3 hBiTangent = Vector3.Cross(hNormal, hTangent);
-
-            Ray GetTangentRay(Vector2 tangentSpacePos)
-            {
-                Vector3 rayOrigin = hit.point +
-                                    (hTangent * tangentSpacePos.x + hBiTangent * tangentSpacePos.y) * radius;
-                rayOrigin += hNormal * 2;
-                Vector3 rayDirection = -hNormal;
-                return new Ray(rayOrigin, rayDirection);
-            }
-
-            List<Pose> hitPoses = new List<Pose>();
-
             // Drawing points
-            foreach (Vector2 point in randomPoints)
+            List<Pose> correctedPlacements = new List<Pose>();
+            foreach (RawPlacement rawPlacement in rawPlacements)
             {
                 // Raycast to find a point on surface. If Ray misses, continue
-                if (!Physics.Raycast(GetTangentRay(point), out RaycastHit ptHit)) continue;
+                if (!Physics.Raycast(
+                    GetTangentRay(mouseRaycastHit, sceneViewCameraTransformUp, rawPlacement.Point2D),
+                    out RaycastHit placementHit
+                )) continue;
 
                 // Calculate rotation and assign to pose with position
-                float randAngDeg = Random.value * 360;
-                Quaternion randRot = Quaternion.Euler(0f, 0f, randAngDeg);
-                Quaternion lookNormal = Quaternion.LookRotation(ptHit.normal);
-                Quaternion rot = lookNormal * (randRot * Quaternion.Euler(90f, 0f, 0f));
-                Pose pose = new Pose(ptHit.point, rot);
-                hitPoses.Add(pose);
+                Quaternion randomRotationZ = Quaternion.Euler(0f, 0f, rawPlacement.AngleDegrees);
+                Quaternion placementHitNormalDirection = Quaternion.LookRotation(placementHit.normal);
+                Quaternion offsetX = Quaternion.Euler(90f, 0f, 0f);
+                Quaternion correctedRotation = placementHitNormalDirection * (randomRotationZ * offsetX);
 
-                // Draw sphere and normal on surface
-                DrawGhostProp(ptHit.point);
-                Handles.DrawAAPolyLine(ptHit.point, ptHit.point + ptHit.normal);
+                Pose correctedPose = new Pose(placementHit.point, correctedRotation);
+                correctedPlacements.Add(correctedPose);
 
-                // Mesh
-                Matrix4x4 poseMatrix = Matrix4x4.TRS(pose.position, pose.rotation, Vector3.one);
-                MeshFilter[] filters = spawnPrefab.GetComponentsInChildren<MeshFilter>();
-                foreach (MeshFilter filter in filters)
-                {
-                    Mesh mesh = filter.sharedMesh;
-                    Material mat = filter.GetComponent<MeshRenderer>().sharedMaterial;
-                    mat.SetPass(0);
-                    Graphics.DrawMeshNow(mesh, pose.position, pose.rotation);
-                }
+                DrawPropMarker(placementHit);
+                DrawPropPreview(correctedPose);
             }
+
+            DrawAxisHandle(mouseRaycastHit, sceneViewCameraTransformUp);
+            DrawLiveCircle(mouseRaycastHit, sceneViewCameraTransformUp);
 
             // Spawn props
-            if (holdingCtrl && Event.current.type == EventType.MouseDown && Event.current.button == 0)
+            if (isHoldingCtrl && Event.current.type == EventType.MouseDown && Event.current.button == 0)
             {
-                TrySpawnProps(hitPoses);
+                PlaceProps(correctedPlacements);
             }
-
-            // Draw Axis handle
-            Handles.color = Color.red;
-            Handles.DrawAAPolyLine(6f, hit.point, hit.point + hTangent);
-            Handles.color = Color.green;
-            Handles.DrawAAPolyLine(6f, hit.point, hit.point + hBiTangent);
-            Handles.color = Color.blue;
-            Handles.DrawAAPolyLine(6f, hit.point, hit.point + hNormal);
-
-            // Draw circle adapted to terrain
-            const int CIRCLE_DETAIL = 256;
-            const float TAU = 6.28318530718f;
-            Vector3[] ringPoints = new Vector3[CIRCLE_DETAIL];
-            for (int i = 0; i < CIRCLE_DETAIL; i++)
-            {
-                float t = i / ((float) CIRCLE_DETAIL - 1);
-                float angRad = t * TAU;
-                Vector2 dir = new Vector2(Mathf.Cos(angRad), Mathf.Sin(angRad));
-                Ray r = GetTangentRay(dir);
-                if (Physics.Raycast(r, out RaycastHit cHit))
-                {
-                    ringPoints[i] = cHit.point + cHit.normal * 0.02f;
-                }
-                else
-                {
-                    ringPoints[i] = r.origin;
-                }
-            }
-
-            Handles.DrawAAPolyLine(ringPoints);
         }
+    }
+
+    private void DrawPropPreview(Pose correctedPlacement)
+    {
+        if (spawnPrefab == null) return;
+        Matrix4x4 targetWorldMatrix = Matrix4x4.TRS(
+            correctedPlacement.position,
+            correctedPlacement.rotation,
+            Vector3.one
+        );
+        MeshFilter[] filters = spawnPrefab.GetComponentsInChildren<MeshFilter>();
+        foreach (MeshFilter filter in filters)
+        {
+            Matrix4x4 propWorldMatrix = filter.transform.localToWorldMatrix;
+            Matrix4x4 finalWorldMatrix = targetWorldMatrix * propWorldMatrix;
+
+            Mesh mesh = filter.sharedMesh;
+            Material mat = filter.GetComponent<MeshRenderer>().sharedMaterial;
+            mat.SetPass(0);
+            Graphics.DrawMeshNow(mesh, finalWorldMatrix);
+        }
+    }
+
+    private void DrawAxisHandle(RaycastHit mouseHit, Vector3 camTfUp)
+    {
+        Handles.color = Color.red;
+        Handles.DrawAAPolyLine(
+            6f,
+            mouseHit.point,
+            mouseHit.point + Vector3.Cross(mouseHit.normal, camTfUp).normalized
+        );
+        Handles.color = Color.green;
+        Handles.DrawAAPolyLine(
+            6f,
+            mouseHit.point,
+            mouseHit.point +
+            Vector3.Cross(mouseHit.normal, Vector3.Cross(mouseHit.normal, camTfUp).normalized)
+        );
+        Handles.color = Color.blue;
+        Handles.DrawAAPolyLine(6f, mouseHit.point, mouseHit.point + mouseHit.normal);
+    }
+
+    private void DrawLiveCircle(RaycastHit mouseHit, Vector3 camTfUp)
+    {
+        // Draw circle adapted to terrain
+        const int CIRCLE_DETAIL = 256;
+        const float TAU = 6.28318530718f;
+        Vector3[] ringPoints = new Vector3[CIRCLE_DETAIL];
+        for (int i = 0; i < CIRCLE_DETAIL; i++)
+        {
+            float t = i / ((float) CIRCLE_DETAIL - 1);
+            float angRad = t * TAU;
+            Vector2 dir = new Vector2(Mathf.Cos(angRad), Mathf.Sin(angRad));
+            Ray r = GetTangentRay(mouseHit, camTfUp, dir);
+            if (Physics.Raycast(r, out RaycastHit cHit))
+            {
+                ringPoints[i] = cHit.point + cHit.normal * 0.02f;
+            }
+            else
+            {
+                ringPoints[i] = r.origin;
+            }
+        }
+
+        Handles.DrawAAPolyLine(ringPoints);
+    }
+
+    private Ray GetTangentRay(RaycastHit mouseHit, Vector3 camTfUp, Vector2 tangentSpacePos)
+    {
+        Vector3 mysteryMath = Vector3.Cross(mouseHit.normal, camTfUp).normalized * tangentSpacePos.x +
+                              Vector3.Cross(
+                                  mouseHit.normal,
+                                  Vector3.Cross(mouseHit.normal, camTfUp).normalized
+                              ) *
+                              tangentSpacePos.y;
+        Vector3 rayOrigin = mouseHit.point + mysteryMath * radius;
+        rayOrigin += mouseHit.normal * 2;
+        Vector3 rayDirection = -mouseHit.normal;
+        return new Ray(rayOrigin, rayDirection);
     }
 
     private void GenerateRandomPoints()
     {
-        randomPoints = new Vector2[spawnCount];
+        rawPlacements = new RawPlacement[spawnCount];
         for (int i = 0; i < spawnCount; i++)
         {
-            randomPoints[i] = Random.insideUnitCircle;
+            rawPlacements[i].Randomize();
         }
     }
 
-    private static void DrawGhostProp(Vector3 pos)
+    private static void DrawPropMarker(RaycastHit h)
     {
-        Quaternion qid = Quaternion.identity;
-        const EventType RP = EventType.Repaint;
-        Handles.SphereHandleCap(-1, pos, qid, 0.1f, RP);
+        Handles.SphereHandleCap(-1, h.point, Quaternion.identity, 0.1f, EventType.Repaint);
+        Handles.DrawAAPolyLine(h.point, h.point + h.normal);
     }
 
-    private void TrySpawnProps(IEnumerable<Pose> poses)
+    private void PlaceProps(IEnumerable<Pose> poses)
     {
         if (spawnPrefab == null) { return; }
 
--- Assets/ExtensionMethods.cs
+++ Assets/ExtensionMethods.cs
@@ -12,9 +12,11 @@ public static class ExtensionMethods
     public static Vector3 Round(this Vector3 v, float s) { return (v / s).Round() * s; }
     public static float Round(this float v, float s) { return Mathf.Round(v / s) * s; }
 
+    // Force a minimum
     public static int AtLeast(this int v, int min) { return Mathf.Max(v, min); }
     public static float AtLeast(this float v, float min) { return Mathf.Max(v, min); }
 
+    // Force a maximum
     public static int AtMost(this int v, int max) { return Mathf.Min(v, max); }
     public static float AtMost(this float v, float max) { return Mathf.Min(v, max); }
 
